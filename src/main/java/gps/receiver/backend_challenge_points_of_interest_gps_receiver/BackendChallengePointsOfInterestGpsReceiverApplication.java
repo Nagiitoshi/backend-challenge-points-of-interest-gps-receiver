9507a667c2003726af48b175cdc47750396f53c3
Nagi <nagiitoshi@gmail.com>
2025-03-11 17:55:08 -0300

    first commit

---
+++ src/main/java/gps/receiver/backend_challenge_points_of_interest_gps_receiver/BackendChallengePointsOfInterestGpsReceiverApplication.java
@@ -0,0 +1,13 @@
+package gps.receiver.backend_challenge_points_of_interest_gps_receiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendChallengePointsOfInterestGpsReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendChallengePointsOfInterestGpsReceiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gps/receiver/backend_challenge_points_of_interest_gps_receiver/BackendChallengePointsOfInterestGpsReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package gps.receiver.backend_challenge_points_of_interest_gps_receiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendChallengePointsOfInterestGpsReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
